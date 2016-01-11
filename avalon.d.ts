cd46636fc217268afdab3d43cf1e781dd1379fb1
luqizheng <luqizheng@126.com>
2016-01-11 10:42:28 +0800

    添加一些api

--- avalon.d.ts
+++ avalon.d.ts
@@ -7,10 +7,37 @@ interface avalonStatic {
     /**
      * define controller
      */
-    define(obj: vm): any
-    filters: any
-    mix(obj1: any, obj2: any, obj3?: any, obj4?: any): any
+    log(obj1: any, obj2: any, obj3?: any, obj4?: any, obj5?: any, obj6?: any, obj7?: any);
+
+    filters: any;
+    vmodels: any;
+    ui: any;
+    errors(s: string);
+    define(obj: any): any;
+    scan(obj?: any);
+    height(ele: any): number;
+    width(ele: any): number;
+    mix(obj1: any, obj2: any, obj3?: any, obj4?: any, obj5?: any, obj6?: any, obj7?: any): any
+    isFunction(s: any): boolean;
+
+    noop: Function;
+
+    ready(fn: Function);
+
+    css(node: any, name: string, value?: string): string;
+    nextTicket(fn: Function);
+    oneObject(obj: any, val?: any);
+    typeof(obj: any);
+    isWindow(obj: any);
+    isPlainObject(obj: any);
+    slice(obj: any, start?: number, end?: number);
+    range(start: number, end: number, step: number);
+    each(obj: any, fn: Function);
+    parseHTML(str): any;
+    innerHTML(node: any, str: string);
+    clearHTML(node);
 }
+
 
 
 declare module "avalon" {
